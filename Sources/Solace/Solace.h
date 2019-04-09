f3affc7c3a32ee6bafb7e2929ebebadcb5a1714a
woxtu <woxtup@gmail.com>
2019-04-10 00:21:12 +0900

    First commit

---
+++ Sources/Solace/Solace.h
@@ -0,0 +1,12 @@
+//
+//  Solace.h
+//  Solace
+//
+//  Created by woxtu on 2019/04/10.
+//  Copyright (c) 2019 woxtu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double SolaceVersionNumber;
+FOUNDATION_EXPORT const unsigned char SolaceVersionString[];
---
+++ Tests/SolaceTests/SolaceTests.swift
@@ -0,0 +1,34 @@
+//
+//  SolaceTests.swift
+//  SolaceTests
+//
+//  Created by woxtu on 2019/04/10.
+//  Copyright (c) 2019 woxtu. All rights reserved.
+//
+
+import XCTest
+@testable import Solace
+
+class SolaceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
